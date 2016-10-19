a16aec4fb03a34f8828117086d1244c26a2d15f6
fcu-d0491050 <joliangel288@gmail.com>
2016-10-20 04:30:04 +0800

    10/19 Excerise 1

---
+++ src/FCU/IECS/Break.java
@@ -0,0 +1,12 @@
+package FCU.IECS;
+
+public class Break {
+	public static void main(String[] args){
+		for(int i = 0; i < 5; i ++) {
+			if(i > 1 && i <= 5 ) {
+				break;
+			}
+			System.out.println(i);
+	}
+ }
+}
